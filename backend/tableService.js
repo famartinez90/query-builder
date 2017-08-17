e97e89b4c7c219dabe129d10d3a38a30f8a9554c
Federico Martinez <fmartinez@oneloop.com.ar>
2017-08-17 14:10:37 -0300

    Generates some methods and routes for table information retrieval

--- backend/tableService.js
+++ backend/tableService.js
@@ -15,11 +15,77 @@ class DatabaseAdapter {
 		});
 	}
 
-	query(statement, callback) {
-		this.connection.query(statement, function (err, result) {
-			if (err) throw err;
-			callback(result);
+	getTables() {
+		let that = this;
+
+		return new Promise(function (accept, reject) {
+			try {
+				that.connection.query('SHOW TABLES;', function (err, result) {
+					if (err) reject(err);
+	
+					let tables = [];
+					for (let table of result) {
+						tables.push(table[Object.keys(table)[0]]);
+					}
+	
+					accept(tables);
+				});
+			} catch (exception) {
+				reject(exception);
+			}
 		});
+	}
+
+	getTable(table) {
+		let that = this;
+		
+		return new Promise(function (accept, reject) {
+			try {
+				that.connection.query(`SELECT * FROM ${table} LIMIT 1;`, function (err, result) {
+					if (err) reject(err);
+					accept(result);
+				});
+			} catch (exception) {
+				reject(exception);
+			}
+		});
+	}
+
+	executeQuery(params) {
+		let statement = this.generateQueryStatement(params);
+		let that = this;
+
+		return new Promise(function (accept, reject) {
+			try {
+				that.connection.query(statement, function (err, result) {
+					if (err) reject(err);
+					accept(result);
+				});
+			} catch (exception) {
+				reject(exception);
+			}
+		});
+	}
+
+	generateQueryStatement(params) {
+		let query = 'SELECT ';
+		
+		query += params.fields !== undefined ? params.fields.join(',') : '*';
+		query += ' ';
+		query += params.from !== undefined ? `FROM ${params.from}` : '';
+		query += ' ';
+		
+		if (params.joins !== undefined) {
+			for (let join of params.joins) {
+				query += `JOIN ${join.table} ON ${join.table}.id = ${params.from}.${join.on} `;
+			}
+		}
+		
+		query += params.where !== undefined ? `WHERE ${params.where}` : 'WHERE 1';
+		query += ' ';
+		query += params.limit !== undefined ? `LIMIT ${params.limit}` : 'LIMIT 10';
+
+		return query+';';
 	}
 }
 
--- index.js
+++ index.js
@@ -2,13 +2,32 @@ const express = require('express')
 const app = express()
 const DatabaseAdapter = require('./backend/tableService.js');
 const dbAdapter = new DatabaseAdapter();
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+app.use(bodyParser.json());
 
 app.listen(2412, function () {
     console.log('Example app listening on port 2412!');
 });
 
 app.get('/tables', function (req, res) {
-    dbAdapter.query('SHOW TABLES', function (result) {
+    dbAdapter.getTables().then(function (result) {
+        res.send(result);
+    });
+});
+
+app.get('/table', function (req, res) {
+    dbAdapter.getTable(req.query.table).then(function (result) {
+        res.send(result);
+    });
+});
+
+app.post('/query', function (req, res) {
+    dbAdapter.executeQuery(req.body).then(function (result) {
         res.send(result);
     });
 });
